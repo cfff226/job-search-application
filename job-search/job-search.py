41e911191938c2a8f52679240deeca209700acfc
cfff226 <cazziednb@outlook.com>
2023-12-17 15:55:19 +0000

    Initial commit

---
+++ job-search/job-search.py
@@ -0,0 +1,28 @@
+# A program which validates the input that a user provides for an application according to the applications criteria
+
+# Required input:
+# Name
+# Age
+# Location
+# Email address
+# Contact number
+# Skills
+# Preferred Job Type
+
+form_complete = False
+
+# while loop to show the form until the form has been completed
+
+    
+    # Request input for users age
+
+    # Request input for users current location
+
+    # Request input for users email address
+
+    # Request input for users contact number
+
+    # Request input for users skills
+
+    # Request input for users preferred job type
+
