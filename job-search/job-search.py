27605fad43b3882a5a5ef6c24aed31a246858792
cfff226 <cazziednb@outlook.com>
2023-12-17 16:18:33 +0000

    Add input to request users name - If the users input is alphabetical, output a temporary message to the user then break out of the loop - Else, continue to ask the user for their name until a valid input has been given

--- job-search/job-search.py
+++ job-search/job-search.py
@@ -14,13 +14,18 @@ form_complete = False
 # while loop to show the form until the form has been completed
 
 while not form_complete:
+    name = input("Please input your name: ")
+    if name.isalpha():
+        print(f"Your name is {name}")
+        break
+    else:
+        continue
+
+    # Request input for users age
     try:
-    # Request input for users name
-        name = str(input("Please input your name: "))
+        age = int(input("Please input your age"))
     except ValueError:
         print("Your input isn't valid")
-    
-    # Request input for users age
 
     # Request input for users current location
 
