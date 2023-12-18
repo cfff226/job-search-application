401a7f26f328336c903a0a6fdb52203f11b3c294
cfff226 <cazziednb@outlook.com>
2023-12-18 09:51:45 +0000

    Declare a boolean variable and set it to false - While the boolean is false, output a list of tech skills to the user - For loop through the range and length of the tech skills list - Ouput each tech skill in the form of a string with a number beside it - Change the boolean varilable to true to exit the while loop

--- job-search/job-search.py
+++ job-search/job-search.py
@@ -9,73 +9,112 @@
 # Skills
 # Preferred Job Type
 
+
+tech_skills = [
+    "Python",
+    "HTML",
+    "CSS",
+    "Javascript",
+    "React",
+    "Angular",
+    "Vue.js",
+    "Node.js",
+    "Ruby",
+    "Lavarel",
+    "Django",
+    "Springboot" "SQL (Structured Query Language",
+    "MySQL",
+    "MongoDB",
+    "Version Control",
+    "Microsoft Azure",
+    "Cloud Computing" "Server Management",
+    "Docker",
+    "Kubernetes",
+    "Flask",
+    "GraphQL",
+    "Jest",
+    "Mocha",
+    "Jasmine",
+    "pytest",
+    "OAuth" "JSON",
+    "Github",
+    "Gitpod",
+]
+
 form_complete = False
 
-# while loop to show the form until the form has been completed
+# # while loop to show the form until the form has been completed
 
-while not form_complete:
-    name = input("Please input your name: ")
-    new_name = name.replace(" ", "")
-    if new_name.isalpha():
-        print(f"Your name is {name}")
-        break
-    else:
-        continue
+# while not form_complete:
+#     name = input("Please input your name: ")
+#     new_name = name.replace(" ", "")
+#     if new_name.isalpha():
+#         print(f"Your name is {name}")
+#         break
+#     else:
+#         continue
 
-while True:
-    # Request input for users age
-    try:
-        age = int(input("Please input your age: "))
-        if age < 18:
-            raise Exception("The minimum age requirement is 16")
-            break
-        elif 18 <= age < 160:
-            print(f"Your age is {age}")
-            break
-    except ValueError:
-        print("Your input isn't valid")
-        continue
+# while True:
+#     # Request input for users age
+#     try:
+#         age = int(input("Please input your age: "))
+#         if age < 18:
+#             raise Exception("The minimum age requirement is 16")
+#             break
+#         elif 18 <= age < 160:
+#             print(f"Your age is {age}")
+#             break
+#     except ValueError:
+#         print("Your input isn't valid")
+#         continue
 
-    # Request input for users current location
+#     # Request input for users current location
 
-while True:
-    location = input("Please input your location: ")
-    new_location = location.replace(" ", "")
-    if not new_location.isalpha():
-        print("Your input isn't valid")
-        continue
-    else:
-        print(f"You have chosen {location} as your location")
-        break
+# while True:
+#     location = input("Please input your location: ")
+#     new_location = location.replace(" ", "")
+#     if not new_location.isalpha():
+#         print("Your input isn't valid")
+#         continue
+#     else:
+#         print(f"You have chosen {location} as your location")
+#         break
 
-while True:
-    # Request input for users email address
-    user_email = input("Please input your email address: ")
-    if "@" and "." in user_email:
-        print(f"Your email address is {user_email}")
-        break
+# while True:
+#     # Request input for users email address
+#     user_email = input("Please input your email address: ")
+#     if "@" and "." in user_email:
+#         print(f"Your email address is {user_email}")
+#         break
 
-while True:
-    # Request input for confirmation of users email address
-    user_email_confirmation = input("Please confirm your email address: ")
-    if user_email != user_email_confirmation:
-        print("The email address's that you have entered do not match")
-        continue
-    else:
-        break
+# while True:
+#     # Request input for confirmation of users email address
+#     user_email_confirmation = input("Please confirm your email address: ")
+#     if user_email != user_email_confirmation:
+#         print("The email address's that you have entered do not match")
+#         continue
+#     else:
+#         break
 
-    # Extend email input to check if email already exists
+#     # Extend email input to check if email already exists
 
-while True:
-    # Request input for users contact number
-    user_phone_number = str(input("Please input your contact number: +"))
-    new_user_phone_number = user_phone_number.replace(" ", "")
-    if new_user_phone_number.isdigit():
-        print("This number contacts only digits")
-        if len(new_user_phone_number) < 13:
-            print("This number is valid")
-            break
+# while True:
+#     # Request input for users contact number
+#     user_phone_number = str(input("Please input your contact number: +"))
+#     new_user_phone_number = user_phone_number.replace(" ", "")
+#     if new_user_phone_number.isdigit():
+#         print("This number contacts only digits")
+#         if len(new_user_phone_number) < 13:
+#             print("This number is valid")
+#             break
+
+skills_listed = False
+
+while skills_listed is False:
+    # Output a menu of list of skills to choose from and the user can add these to a list
+
+    for i in range(len(tech_skills)):
+        print(str(i + 1) + ". " + tech_skills[i])
+        skills_listed = True
 
     # Request input for users skills
-
-    # Request input for users preferred job type
